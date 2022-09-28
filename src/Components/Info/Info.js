8bd9c440b7f03441cf04f06494e2f18900e224de
Sifat-Khan-cr <pskcrjr@gmail.com>
2022-09-29 00:31:54 +0600

    break added

--- src/Components/Info/Info.js
+++ src/Components/Info/Info.js
@@ -5,6 +5,17 @@ const Info = () => {
     return (
         <div className='mt-10'>
             <Personal></Personal>
+            <div>
+                <h1 className='text-4xl mt-10 mb-5'>Add a break</h1>
+                <div className='flex flex-row justify-evenly bg-cyan-100 p-5 mx-10'>
+                    <button className='btn btn-circle'>10s</button>
+                    <button className='btn btn-circle'>20s</button>
+                    <button className='btn btn-circle'>30s</button>
+                    <button className='btn btn-circle'>40s</button>
+                    <button className='btn btn-circle'>50s</button>
+                </div>
+            </div>
+
 
 
         </div>
